242b4ae46fa4e83b028d29676f2682c3a097e97d
Miaobing <mjiang@forzadata.com>
2021-03-01 15:53:42 +0800

    upgrade shardingsphere-jdbc to 5.0.5-alpha

--- src/main/java/com/forzadata/bodytrack/TransactionConfiguration.java
+++ src/main/java/com/forzadata/bodytrack/TransactionConfiguration.java
@@ -3,14 +3,15 @@ package com.forzadata.bodytrack;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-//@Configuration
-//@EnableTransactionManagement
+@Configuration
+@EnableTransactionManagement
 public class TransactionConfiguration {
 
     @Bean
@@ -21,5 +22,10 @@ public class TransactionConfiguration {
     @Bean
     public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedJdbcTemplate(final DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
